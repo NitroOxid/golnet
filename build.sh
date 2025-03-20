bff7eff89cba23e9d8a77b8a2940001972655ff5
NitroOxid <nitro138@gmail.com>
2025-03-20 22:56:02 +0300

    v0.2 Added check freeze server; Building for windows

--- build.sh
+++ build.sh
@@ -6,4 +6,10 @@
 # ./golnet 127.0.0.1 8081 you_password 'say "[FF0000]Checking the server label[-] without panic."'
 
 go clean -cache
+
+# Linux build
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o golnet -ldflags="-w -s" golnet.go
+
+# Windows build
+#CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o golnet.exe -ldflags="-w -s" golnet.go
+
--- golnet.go
+++ golnet.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -19,6 +20,7 @@ func main() {
 	password := os.Args[3]
 	command := os.Args[4]
 
+	// Connecting to server
 	conn, err := net.Dial("tcp", ip+":"+port)
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
@@ -28,17 +30,57 @@ func main() {
 
 	// Reading initial server response
 	reader := bufio.NewReader(conn)
-	_, _ = reader.ReadString('\n')
 
 	// Sending password
 	fmt.Fprintf(conn, password+"\n")
-	time.Sleep(500 * time.Millisecond)
+	_, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error during login:", err)
+		os.Exit(1)
+	}
 
 	// Sending command
 	fmt.Fprintf(conn, command+"\n")
-	time.Sleep(500 * time.Millisecond)
 
-	// Sending exit command
-	fmt.Fprintf(conn, "exit\n")
-	time.Sleep(500 * time.Millisecond)
+	// Timeout waiting for a response
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+
+	// Waiting for command string
+	expected := fmt.Sprintf("INF Executing command '%s' by Telnet", command)
+
+	// Flag to check if the command string has been received
+	responseReceived := false
+
+	// Reading data from connection
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			// If the socket is closed or there is an EOF error, we exit
+			if strings.Contains(err.Error(), "closed") || strings.Contains(err.Error(), "EOF") {
+				break
+			}
+			// If there is another error, we display it
+			fmt.Println("Error reading from server:", err)
+			break
+		}
+
+		// Printing received strings
+		//fmt.Printf("Received line: %s", line)
+
+		// If the string contains a command, mark the command as executed
+		if strings.Contains(line, expected) {
+			responseReceived = true
+			break
+		}
+	}
+
+	// If the command was executed
+	if responseReceived {
+		fmt.Println("Command was executed successfully")
+		os.Exit(0)
+	}
+
+	// If time has passed and no response was received
+	fmt.Println("Server is unresponsive. It might be frozen.")
+	os.Exit(4)
 }
